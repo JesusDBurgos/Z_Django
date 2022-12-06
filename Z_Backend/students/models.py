97ffd87055ae0c409831aeadab62b4c60f6512c1
ivandjt226 <ivandjt226@gmail.com>
2022-12-06 18:04:26 -0500

    Creación del modelo estudiante

--- Z_Backend/students/models.py
+++ Z_Backend/students/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    studentId = models.AutoField(primary_key=True)
+    Name = models.CharField("Nombre estudiante", max_length=100)
+
+    Emotion_Select = (
+        ('H', 'Happy'),
+        ('S', 'Sad'),
+        ('A', 'Angry'),
+        ('N', 'Neutral'),
+    )
+
+    Emotion = models.CharField("Emoción", max_length=100, choices=Emotion_Select, default='N')
+    Age = models.IntegerField("Edad", max_length=3)
+
+    Gender_Select = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    gender = models.CharField("Sexo", max_length=1, choices=Gender_Select, default='M')
+    CreatedAt = models.DateTimeField("Creado a", auto_now_add=True)
