a1bdb88106967e8eadc0a7ed36a8f9293545adf5
Ivandjt226 <ivandjt226@gmail.com>
2022-12-15 04:07:03 -0500

    Creación del componente de la barra de navegación del frontend

---
+++ z_frontend/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+//import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from "./components/Navigation";
+
+function App() {
+  return (
+    //Importo el componente de la barra de navegación
+    <Navigation />
+  );
+}
+
+export default App;
