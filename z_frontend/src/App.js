dab22423895ac45c548315a334ae6fc4b2982d4f
ivandjt226 <ivandjt226@gmail.com>
2023-06-05 06:08:22 -0500

    Update App.js con el sistema CRUD para administrar usuarios

--- z_frontend/src/App.js
+++ z_frontend/src/App.js
@@ -7,7 +7,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./components/Home";
 import Chatbot from "./components/Chatbot";
-
+import Users  from './components/Users';
 
 function App() {
   return (
@@ -18,6 +18,7 @@ function App() {
        <Routes>
          <Route exact path="/" element={<Home/>} />
          <Route exact path="/Elaine_Assistant" element={<Chatbot/>} />
+         <Route exact path="/Admin" element={<Users/>} />
        </Routes>
     </BrowserRouter>
   );
