cbb242619da4dc607668651e4f69af8be40a7b2f
ivandjt226 <ivandjt226@gmail.com>
2023-06-15 00:59:31 -0500

    Creación de endpoints del CRUD de usuarios

--- Z_Vision/src/management.py
+++ Z_Vision/src/management.py
@@ -1,8 +1,40 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+
+
+
+
+#Desplegando las endpoints
+@app.route('/api/users', methods=['GET'])
+def api_get_users():
+    return jsonify(get_users())
+
+@app.route('/api/users/<user_id>', methods=['GET'])
+def api_get_user(user_id):
+    return jsonify(get_user_by_id(user_id))
+
+@app.route('/api/users/add',  methods = ['POST'])
+def api_add_user():
+    user = request.get_json()
+    return jsonify(insert_user(user))
+
+@app.route('/api/users/update',  methods = ['PUT'])
+def api_update_user():
+    user = request.get_json()
+    return jsonify(update_user(user))
+
+@app.route('/api/users/delete/<user_id>',  methods = ['DELETE'])
+def api_delete_user(user_id):
+    return jsonify(delete_user(user_id))
 
 #Creación  de ruta del servidor
 @app.route('/')
