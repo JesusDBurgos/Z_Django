790f10ea507a1379058c8e9ff373fa443c893205
ivandjt226 <ivandjt226@gmail.com>
2023-06-15 00:43:41 -0500

    Creando usuarios de prueba

--- Z_Vision/src/management.py
+++ Z_Vision/src/management.py
@@ -10,6 +10,25 @@ def hello_world():
     return 'Hello, World!'
 
 
+# Usuarios de prueba
+users = []
+user0 = {
+    "name": "Charles Effiong",
+    "age": "48",
+    "gender": "Masculino",
+    "gender": "Enojo"
+}
+
+user1 = {
+    "name": "Samantha Adebanjo",
+    "age": "25",
+    "gender": "Femenino",
+    "gender": "Felicidad"
+}
+
+
+
+#Despliegue del servidor
 if __name__ == "__main__":
     #app.debug = True
     #app.run(debug=True)
