8af392cfae42c384e943b85ebc5663a2b5ad20b2
ivandjt226 <ivandjt226@gmail.com>
2023-06-15 02:19:13 -0500

    Creación del modelo de usuarios

--- Z_Vision/src/management.py
+++ Z_Vision/src/management.py
@@ -1,12 +1,38 @@
 import os
 
+#Librerías para levantar el servidor y retornar las solicitudes HTTPS
 from flask import Flask, jsonify, request
+
+#Librerías para crear el modelo de usuario
+from flask_marshmallow import Marshmallow
+from marshmallow import Schema, fields, Model, Column, Integer, String, DateTime
+import datetime
+
+#Librerías para enviar datos al Frontend
 from flask_cors import CORS
 
 
 app = Flask(__name__)
 
+ma=Marshmallow(app)
+
 CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+#Creación del modelo usuario
+class Usuarios(Model):
+    user_id = Column(Integer,primary_key=True)
+    name    = Column(String(100))
+    age     = Column(Integer(100))
+    gender  = Column(String(100))
+    emotion = Column(String(100))
+    date    = Column(DateTime,default=datetime.datetime.now)
+
+    def __init__(self,name,age, gender, emotion):
+        self.name=name
+        self.age=age
+        self.gender=gender
+        self.emotion=emotion
 
 
 # creación de funciones
@@ -30,17 +56,22 @@ def delete_user():
     pass
 
 
-#Desplegando las endpoints
+#Desplegando los endpoints
+
 @app.route('/v1/api/users', methods=['GET'])
 # Esta función permite listar los usuarios
 def api_get_users():
     return users    
 #    return jsonify(get_users())
 
-@app.route('/v1/api/users/<user_id>', methods=['GET'])
+@app.route('/v1/api/users/{user_id}', methods=['GET'])
 # Esta función permite listar los usuarios por id
-def api_get_user(user_id):
-    return jsonify(get_user_by_id(user_id))
+def api_get_user(user_id: Integer):
+    for user in users:
+        if user["id"]== id:
+            return user
+    return "La reservación no existe"
+#    return jsonify(get_user_by_id(user_id))
 
 @app.route('/v1/api/users/add',  methods = ['POST'])
 def api_add_user():
@@ -57,8 +88,6 @@ def api_delete_user(user_id):
     return jsonify(delete_user(user_id))
 
 #Creación  de ruta del servidor
-# Revisar como poner tags para documentar en Swagger
-#@app.route('/v1/Bienvenida', tags=["Home"]) 
 @app.route('/v1/Bienvenida')
 def home():
     return "Bienvenidos a la API de Ingeniería Mecatronica UNAB"
@@ -67,17 +96,21 @@ def home():
 # Usuarios de prueba
 users = []
 user0 = {
+    "user_id": "001",
     "name": "Charles Effiong",
     "age": "48",
     "gender": "Masculino",
-    "emotion": "Enojo"
+    "emotion": "Enojo",
+    "date" : "05-05-2025"
 }
 
 user1 = {
+    "user_id": "002",
     "name": "Samantha Adebanjo",
     "age": "25",
     "gender": "Femenino",
-    "emotion": "Felicidad"
+    "emotion": "Felicidad",
+    "date" : "06-05-2025"
 }
 
 users.append(user0)
