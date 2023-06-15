5dcafbdd57adde3060e5efc6e867a0ca97d6a97a
ivandjt226 <ivandjt226@gmail.com>
2023-06-15 15:24:15 -0500

    Modificación modelo usuario

--- Z_Vision/src/Model_user.py
+++ Z_Vision/src/Model_user.py
@@ -9,16 +9,13 @@ class Users(db.Model):
     __tablename__ = 'users'
 
     id      = db.Column(db.Integer, primary_key=True)
-    age     = db.Column(db.Integer(100), nullable=False)
+    age     = db.Column(db.Integer, nullable=False)
     gender  = db.Column(db.String(15), nullable=False)
     emotion = db.Column(db.String(20), nullable=False)
     date    = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def json(self):
         return {'id': self.id, 'age': self.age, 'gender': self.gender, 'emotion': self.emotion}
-
-db.init_app()
-db.create_all()
 
 '''
 '''
