7f9b1d9e456930b79a3231b6b9f626647a5efb65
ivandjt226 <ivandjt226@gmail.com>
2023-06-16 15:17:15 -0500

    Verificar creación de base de datos

--- Z_Vision/src/app.py
+++ Z_Vision/src/app.py
@@ -2,24 +2,28 @@
 import sqlite3
 
 def connect_to_db():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect("Userdatabase.db")
     return conn
 
 def create_db_table():
     try:
+#        tables = [
+#
+#        ]
         conn = connect_to_db()
-        conn.execute('''
+        conn.execute("""
             CREATE TABLE users (
-                user_id INTEGER PRIMARY KEY NOT NULL,
+                user_id INTEGER PRIMARY KEY AUTOINCREMENT,
                 age TEXT NOT NULL,
                 gender TEXT NOT NULL,
                 emotion TEXT NOT NULL
             );
-        ''')
+        """)
 
         conn.commit()
         print("La creación de la tabla de usuarios ha sido exitosa")
-    except:
+  
+    except sqlite3.OperationalError:
         print("La creación de la tabla de usuarios ha fallado")
     finally:
         conn.close()
