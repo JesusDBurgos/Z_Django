b2e73769f275ae392f2ef3743d6bff1bd9ba3b4f
ivandjt226 <ivandjt226@gmail.com>
2023-06-17 20:18:14 -0500

    Ajustando el id del controlador y el archivo principal

--- Z_Vision/src/app_controller.py
+++ Z_Vision/src/app_controller.py
@@ -13,32 +13,32 @@ def create_user(age, gender, emotion, DateCreated):
 def get_users():
     db = connect_to_db()
     cursor = db.cursor()
-    query = "SELECT user_id, age, gender, emotion, DateCreated FROM users"
+    query = "SELECT id, age, gender, emotion, DateCreated FROM users"
     cursor.execute(query)
     return cursor.fetchall()
 
 #Listar usuario por id en la base de datos
-def get_user_by_id(user_id):
+def get_user_by_id(id):
     db = connect_to_db()
     cursor = db.cursor()
-    statement = "SELECT user_id, age, gender, emotion, DateCreated FROM users WHERE user_id = ?"
-    cursor.execute(statement, [user_id])
+    statement = "SELECT id, age, gender, emotion, DateCreated FROM users WHERE id = ?"
+    cursor.execute(statement, [id])
     return cursor.fetchone()
 
 #Actualizar usuario por id en la base de datos
-def update_user(user_id, age, gender, emotion, DateCreated):
+def update_user(id, age, gender, emotion, DateCreated):
     db = connect_to_db()
     cursor = db.cursor()
-    statement = "UPDATE users SET age = ?, gender = ?, emotion = ? , DateCreated = ? WHERE user_id = ?"
-    cursor.execute(statement, [age, gender, emotion, DateCreated, user_id])
+    statement = "UPDATE users SET age = ?, gender = ?, emotion = ? , DateCreated = ? WHERE id = ?"
+    cursor.execute(statement, [age, gender, emotion, DateCreated, id])
     db.commit()
     return True
 
 #Eliminar usuario por id en la base de datos
-def delete_user(user_id):
+def delete_user(id):
     db = connect_to_db()
     cursor = db.cursor()
-    statement = "DELETE FROM users WHERE user_id = ?"
-    cursor.execute(statement, [user_id])
+    statement = "DELETE FROM users WHERE id = ?"
+    cursor.execute(statement, [id])
     db.commit()
     return True
