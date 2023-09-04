0af78581e2a8af94928233d4ac7a45bea1e60b2b
ivandjt226 <ivandjt226@gmail.com>
2023-09-04 17:33:27 -0500

    Validación del fetch con datos de prueba

---
+++ z_frontend/src/components/DataUser.js
@@ -0,0 +1,8 @@
+const DataUser = 
+[ 
+    {id:1, age:24, gender:'F', emotion:'Triste', DateCreated:'02-01-2023'},
+    {id:2, age:22, gender:'M', emotion:'Alegre', DateCreated:'02-02-2023'},
+    {id:3, age:34, gender:'F', emotion:'Enojo',  DateCreated:'02-03-2023'},
+];
+  
+export default DataUser
