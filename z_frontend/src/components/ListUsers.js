2f44aea6db3902cd76b65973e078663d87ae7726
ivandjt226 <ivandjt226@gmail.com>
2023-09-04 04:38:39 -0500

    Agregando la funcion ListUsers desde el  frontend

--- z_frontend/src/components/ListUsers.js
+++ z_frontend/src/components/ListUsers.js
@@ -1,2 +1,15 @@
-//import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import 'react-bootstrap';
 
+const data = 
+[ 
+{id:1, age:22,gender:'M',emotion:'Alegre',DateCreated:'02-02-2023'},
+{id:2, age:24,gender:'F',emotion:'Triste',DateCreated:'02-01-2023'},
+];
+
+function ListUsers(){
+
+
+}
+
+export default ListUsers;
