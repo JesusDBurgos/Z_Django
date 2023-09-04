93522e5240bd63bf967accfe2c124ab8e9fd4c7a
ivandjt226 <ivandjt226@gmail.com>
2023-09-04 04:02:35 -0500

    Agregar componente CORS en el Servidor

--- Z_Vision/src/Api.py
+++ Z_Vision/src/Api.py
@@ -56,7 +56,7 @@ def api_delete_user(id):
 
 """
 Enable CORS. Disable it if you don't need CORS
-
+# Notas
 @app.after_request
 def after_request(response):
     response.headers["Access-Control-Allow-Origin"] = "*" # <- You can change "*" for a domain for example "http://localhost"
@@ -65,7 +65,13 @@ def after_request(response):
     response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
     return response
 """
-
+@app.after_request
+def after_request(response):
+    response.headers["Access-Control-Allow-Origin"] = "*" # <- You can change "*" for a domain for example "http://localhost"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, PUT, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization"
+    return response
 
 if __name__ == "__main__":
     create_db_table()
