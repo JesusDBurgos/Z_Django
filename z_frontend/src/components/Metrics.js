26cc3b1ef4b8e831e8963ebd6b4da02f421fcb35
ivandjt226 <ivandjt226@gmail.com>
2023-09-07 18:53:42 -0500

    Organización de estilo de la pantalla de  metricas

--- z_frontend/src/components/Metrics.js
+++ z_frontend/src/components/Metrics.js
@@ -6,7 +6,7 @@ function Metrics() {
   return (
     <div>
       <div className='p-3 d-flex justify-content-around mt-3'>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 me-2'>
           <div className='text-center pb-1'>
             <h4>Usuarios</h4>
           </div>
@@ -15,7 +15,7 @@ function Metrics() {
             <h5>Cantidad: </h5>
           </div>
         </div>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 me-2'>
           <div className='text-center pb-1'>
             <h4>Conversaciones</h4>
           </div>
@@ -24,7 +24,7 @@ function Metrics() {
             <h5>Cantidad: </h5>
           </div>
         </div>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 me-2'>
           <div className='text-center pb-1'>
             <h4>Emociones Positivas</h4>
           </div>
@@ -33,7 +33,7 @@ function Metrics() {
             <h5>Total: </h5>
           </div>
         </div>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 me-2'>
           <div className='text-center pb-1'>
             <h4>Emociones Negativas</h4>
           </div>
