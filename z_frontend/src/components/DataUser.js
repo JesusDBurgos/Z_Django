c0e00df29d70f365729e2066b29154ad413f582e
ivandjt226 <ivandjt226@gmail.com>
2023-09-18 03:00:14 -0500

    Estructura de datos de prueba nuevos

--- z_frontend/src/components/DataUser.js
+++ z_frontend/src/components/DataUser.js
@@ -1,8 +1,8 @@
 const DataUser = 
 [ 
-    {id:12345678, Age:'24', Gender:'F', Emotion:'Triste', DateCreated:'02-01-2023'},
-    {id:23456789, Age:'22', Gender:'M', Emotion:'Alegre', DateCreated:'02-02-2023'},
-    {id:34567890, Age:'34', Gender:'F', Emotion:'Enojo',  DateCreated:'02-03-2023'},
+    {id:10, Age:'24', Gender:'F', Emotion:'Tristeza', DateCreated:'02-01-2023'},
+    {id:11, Age:'22', Gender:'M', Emotion:'Felicidad', DateCreated:'02-02-2023'},
+    {id:12, Age:'34', Gender:'F', Emotion:'Enojo',  DateCreated:'02-03-2023'},
 ];
   
 export default DataUser
