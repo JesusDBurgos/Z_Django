89166e4f7fd1df9ea01bf7e2d8e0b059f337cf9e
ivandjt226 <ivandjt226@gmail.com>
2023-09-19 04:10:19 -0500

    Separación de archivos obsoletos

--- z_frontend/src/components/Deprecados/CreateUser.js
+++ z_frontend/src/components/Deprecados/CreateUser.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DataUser from './DataUser';
+import DataUser from '../DataUser';
 import { v4 as uuid } from 'uuid';
 import { Link, useNavigate } from 'react-router-dom';
   
