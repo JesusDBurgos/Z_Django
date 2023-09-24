ba154686ecbe49dfaa6aa4dcc4b42df1adf60a33
ivandjt226 <ivandjt226@gmail.com>
2023-09-24 16:51:31 -0500

    Agregamos la visualización de data desde una tabla

--- z_frontend/src/components/RData.js
+++ z_frontend/src/components/RData.js
@@ -12,7 +12,46 @@ function RData() {
       </p>
     </li>
   );
-  return <ul>{listUsers}</ul>;
+  const Rusers = DataUser.map(Duser =>
+    <tr key={Duser.id}>
+        <td>{Duser.id}</td>
+        <td>{Duser.Age}</td>
+        <td>{Duser.Gender}</td>
+        <td>{Duser.Emotion}</td>
+        <td>{Duser.DateCreated}</td>
+        <td>
+        <button className='btn btn-secondary btn-sm btn-block me-2'>Actualizar</button>
+        <button className='btn btn-danger btn-sm btn-block me-2'>Eliminar</button>
+        </td>
+    </tr>  
+  );
+  return (
+    <div className="App container m-4">
+      <div className="row" style={{ marginTop: '4rem' }}>
+       <div className="text-center">
+         <h1>Trayendo datos al frontend React.</h1>
+         <ul>{listUsers}</ul>
+       </div>
+       <div className="col-md-8" >   
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>Index</th>
+              <th>Edad</th>
+              <th>Genero</th>
+              <th>Emoción detectada</th>
+              <th>Fecha de creación</th>
+              <th>Operaciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Rusers}
+          </tbody>
+        </table>
+      </div>
+      </div>
+    </div>
+  );
 }
 
 export default RData;
