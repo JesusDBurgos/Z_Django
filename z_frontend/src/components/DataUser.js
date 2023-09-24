49216f6f5526e316e591757eed8afa05cdfa0827
ivandjt226 <ivandjt226@gmail.com>
2023-09-24 16:15:13 -0500

    Validamos un diccionario para la visualización de datos en el interfaz

--- z_frontend/src/components/DataUser.js
+++ z_frontend/src/components/DataUser.js
@@ -1,8 +1,8 @@
 const DataUser = 
 [ 
-    {id:01, Age:'24', Gender:'F', Emotion:'Tristeza', DateCreated:'02-01-2023'},
-    {id:02, Age:'22', Gender:'M', Emotion:'Felicidad', DateCreated:'02-02-2023'},
-    {id:03, Age:'34', Gender:'F', Emotion:'Enojo',  DateCreated:'02-03-2023'},
+    {id:1, Age:'24', Gender:'F', Emotion:'Tristeza', DateCreated:'02-01-2023'},
+    {id:2, Age:'22', Gender:'M', Emotion:'Felicidad', DateCreated:'02-02-2023'},
+    {id:3, Age:'34', Gender:'F', Emotion:'Enojo',  DateCreated:'02-03-2023'},
 ];
 
 //id is used in JSX as a key
--- z_frontend/src/components/RData.js
+++ z_frontend/src/components/RData.js
@@ -1,13 +1,14 @@
-import { DataUser } from './DataUser';
+//import { DataUser } from './components/DataUser';
+import DataUser from './DataUser';
 
 export default function RData() {
   const listUsers = DataUser.map(Duser =>
     <li key={Duser.id}>
       <p>
-        <b>{Duser.Emotion}</b>
-          Tiene una edad de:          {' ' + Duser.Age + ' '}
-          Un genero de:               {' ' + Duser.Gender + ' '}
-          Participo en el app el día: {' ' + Duser.DateCreated + ' '}
+          El usuario tiene una emoción de <b>{Duser.Emotion}</b>
+          , tiene una edad de {' ' + Duser.Age + ' '} años, 
+          un genero {' ' + Duser.Gender + ' '} (F:Femenino/M:Masculino) y
+          participo en el app el día: {' ' + Duser.DateCreated + ' '}
       </p>
     </li>
   );
