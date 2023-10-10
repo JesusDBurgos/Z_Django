17dcbbded546d124e6837c895488846fe4acf384
ivandjt226 <ivandjt226@gmail.com>
2023-10-10 06:15:40 -0500

    Validación de comunicación de APIS

--- z_frontend/src/App.js
+++ z_frontend/src/App.js
@@ -12,8 +12,8 @@ import Home from "./components/Home";
 import Chatbot from "./components/Chatbot";
 import Vision_Camera from './components/Vision_Camera';
 
-//import ReadAPI  from './components/ReadAPI';
-import RData from './components/RData';
+import ReadAPI  from './components/ReadAPI';
+//import RData from './components/RData';
 import ListUser from './components/ListUser';
 import Users  from './components/Users';
 import Metrics from './components/Metrics';
@@ -30,7 +30,7 @@ function App() {
          <Route exact path="/" element={<Home/>} />
          <Route path="/Elaine_Assistant" element={<Chatbot/>} />
          <Route path="/Vision_camera" element={<Vision_Camera/>} />
-         <Route path="/API_conect" element={<RData/>} />
+         <Route path="/API_conect" element={<ReadAPI/>} />
          <Route exact path="/List" element={<ListUser/>} />
          <Route path="/Manage" element={<Users/>} />
          <Route path="/Metrics" element={<Metrics/>} />
