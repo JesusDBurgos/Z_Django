9759828a0628839e2eaa912ff4b8d836ceef2dcf
ivandjt226 <ivandjt226@gmail.com>
2023-10-10 09:58:16 -0500

    Creación de variable de separación de emociones

--- z_frontend/src/components/Metrics.js
+++ z_frontend/src/components/Metrics.js
@@ -7,8 +7,8 @@ function Metrics() {
   const getUsers = async () => {
       const res = await fetch('http://localhost:8000/api/v1/users');
       const data = await res.json();
-      console.log(data)
-      console.log(data.length)
+      //console.log(data)
+      //console.log(data.length)
         
       setUsers(data);
     };
@@ -17,6 +17,49 @@ function Metrics() {
       getUsers();
     }, []);
   
+  const Emotions = users.map((Emo,key) =>
+      <p key={key}>{Emo[3]}</p>
+    )
+
+  var Em_Neg=0;
+  var Em_Pos=0;
+
+  console.log(Emotions)
+
+  if (Emotions == 'Tristeza' || Emotions == 'Enojo' || Emotions == 'Ira' ){
+    Em_Neg = Em_Neg + 1;
+    console.log(Em_Neg)
+  } else if (Emotions == 'Sorpresa' || Emotions == 'Felicidad' || Emotions == 'Neutral')
+  {
+    Em_Pos = Em_Pos + 1;
+    console.log(Em_Pos) 
+  };
+  
+  /*
+  switch(Emotions){
+    case 'Tristeza':
+      Em_Neg = Em_Neg + 1;
+      console.log(Em_Neg)
+    case 'Enojo': 
+      Em_Neg = Em_Neg + 1;
+      console.log(Em_Neg)
+    case 'Ira':
+      Em_Neg = Em_Neg + 1;
+      console.log(Em_Neg)
+    case 'Sorpresa':
+      Em_Neg = Em_Neg + 1;
+      console.log(Em_Neg)
+    case 'Felicidad': 
+      Em_Pos = Em_Pos + 1;
+      console.log(Em_Pos)
+    case 'Neutral':
+      Em_Pos = Em_Pos + 1;
+      console.log(Em_Pos)
+  }
+  */
+
+  //console.log(Emotions)
+
   return (
     <div>
       <div className='p-3 d-flex justify-content-around mt-3'>
@@ -45,7 +88,7 @@ function Metrics() {
           </div>
           <hr />
           <div className=''>
-            <h5>Total: </h5>
+            <h5>Total: {Em_Pos}</h5>
           </div>
         </div>
         <div className='px-3 pt-2 pb-3 border shadow-sm w-25 me-2'>
@@ -54,11 +97,13 @@ function Metrics() {
           </div>
           <hr />
           <div className=''>
-            <h5>Total: </h5>
+            <h5>Total: {Em_Neg}</h5>
           </div>
-        </div>
+        </div>  
       </div>
+      <ul>{Emotions}</ul>
     </div>
+    
   )
 }
 
