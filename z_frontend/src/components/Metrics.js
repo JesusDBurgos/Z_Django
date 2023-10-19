78f217591823106b4f81ba17259029fd7367fd3f
ivandjt226 <ivandjt226@gmail.com>
2023-10-19 00:49:28 -0500

    Validamos los componentes del vector de Emotions

--- z_frontend/src/components/Metrics.js
+++ z_frontend/src/components/Metrics.js
@@ -17,24 +17,48 @@ function Metrics() {
       getUsers();
     }, []);
   
+  const Edades = users.map((Ed,key) =>
+  <p key={key}>{Ed[1]}</p>
+  )
+
+  const Genero= users.map((Gen,key) =>
+  <p key={key}>{Gen[2]}</p>
+  )  
+
+
   const Emotions = users.map((Emo,key) =>
       <p key={key}>{Emo[3]}</p>
-    )
+  )
 
-  var Em_Neg=0;
-  var Em_Pos=0;
+  //console.log(Emotions)
 
-  console.log(Emotions)
+  function Conteo(props){
+    var Em_Neg=0;
+    var Em_Pos=0;
 
-  if (Emotions == 'Tristeza' || Emotions == 'Enojo' || Emotions == 'Ira' ){
-    Em_Neg = Em_Neg + 1;
-    console.log(Em_Neg)
-  } else if (Emotions == 'Sorpresa' || Emotions == 'Felicidad' || Emotions == 'Neutral')
-  {
-    Em_Pos = Em_Pos + 1;
-    console.log(Em_Pos) 
-  };
-  
+    //var arr = Emo[3];
+    const Em = props.Em;
+    for (var i = 0; i <= Emotions.length; i++) {
+       console.log(Emotions[i]);
+    
+    if (Emotions === 'Tristeza' || Emotions === 'Enojo' || Emotions === 'Ira' ){
+      Em_Neg = Em_Neg + 1;
+      //console.log(Em_Neg)  
+    } else if (Emotions === 'Sorpresa' || Emotions === 'Felicidad' || Emotions === 'Neutral')
+    {
+      Em_Pos = Em_Pos + 1;
+      //console.log(Em_Pos)
+    };
+   }
+   if (Em){
+    return Em_Pos
+   }
+   else{
+    return Em_Neg
+   } 
+  }
+
+
   /*
   switch(Emotions){
     case 'Tristeza':
@@ -88,7 +112,7 @@ function Metrics() {
           </div>
           <hr />
           <div className=''>
-            <h5>Total: {Em_Pos}</h5>
+            <h5>Total: <Conteo Em={true} /> </h5>
           </div>
         </div>
         <div className='px-3 pt-2 pb-3 border shadow-sm w-25 me-2'>
@@ -97,11 +121,16 @@ function Metrics() {
           </div>
           <hr />
           <div className=''>
-            <h5>Total: {Em_Neg}</h5>
+            <h5>Total: <Conteo Em={false} /></h5>
           </div>
         </div>  
       </div>
+      <ul>{Edades}</ul>
+      <br></br>
+      <ul>{Genero}</ul>
+      <br></br>
       <ul>{Emotions}</ul>
+      <br></br>
     </div>
     
   )
