30b4ffdd7c25e8f156d0626250cc13e8f2d96e92
ivandjt226 <ivandjt226@gmail.com>
2023-11-24 03:11:51 -0500

    Estructura de pagina de la app

--- z_frontend/src/components/ListUser.js
+++ z_frontend/src/components/ListUser.js
@@ -6,64 +6,60 @@ import React, { useEffect, useState } from 'react'
 //Petición con Fetch
 function ListUser() {
 
-    const [users, setUsers] = useState([]); 
+  const [users, setUsers] = useState([]);
 
-    // Modify the current state by setting the new data to
-    // the response from the backend
+  // Modify the current state by setting the new data to
+  // the response from the backend
 
-    const getUsers = async () => {
-        const res = await fetch('http://localhost:8000/api/v1/users');
-        //console.log(res)
-        const data = await res.json();
-        //console.log(data)
-        //console.log(data.length)
-        //const id = data[0];
-        //console.log(id)
-        //result => setUsers(result.data);
-        
-        setUsers(data);
-      };
-    
-    useEffect(() => {
-        getUsers();
-      }, []);
-    
-    //console.log(users)  
+  const getUsers = async () => {
+    const res = await fetch('http://localhost:8000/api/v1/users');
+    //console.log(res)
+    const data = await res.json();
+    //console.log(data)
+    //console.log(data.length)
+    //const id = data[0];
+    //console.log(id)
+    //result => setUsers(result.data);
 
-    return (
-     <div className="App container m-6">
-        <div className="row" style={{ marginTop: '4rem' }}>
-          <div className="text-center">
-          <h1>Connectando el Frontend de React hacia el Backend de Flask.</h1>
-          
-        <div className="col-md-10" >   
-        <table className="table table-striped">
-          <thead>
-            <tr>
-              <th>Index</th>
-              <th>Edad</th>
-              <th>Genero</th>
-              <th>Emoción detectada</th>
-              <th>Fecha creación</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user,key) =>
-            <tr key={key}>
-            <td>{user[0]}</td>
-            <td>{user[1]}</td>
-            <td>{user[2]}</td>
-            <td>{user[3]}</td>
-            <td>{user[4]}</td>
-          </tr>
-        )}
-          </tbody>
-        </table>
+    setUsers(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  //console.log(users)  
+
+  return (
+    <><h1 className="bg-info text-center font-monospace fw-bold lh-base">Listado de usuarios</h1>
+      <div className="d-flex flex-wrap justify-content-evenly" style={{ marginTop: '3rem' }}>
+        <div className="col-md-10" >
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th>Index</th>
+                <th>Edad</th>
+                <th>Genero</th>
+                <th>Emoción detectada</th>
+                <th>Fecha creación</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, key) =>
+                <tr key={key}>
+                  <td>{user[0]}</td>
+                  <td>{user[1]}</td>
+                  <td>{user[2]}</td>
+                  <td>{user[3]}</td>
+                  <td>{user[4]}</td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
-      </div>
-      </div>
-    </div>
-      );
+    </>
+  );
 
 }
 export default ListUser
