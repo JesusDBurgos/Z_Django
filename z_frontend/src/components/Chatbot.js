05ab3d3d25293828225c21cd3a03cb9ffd8cf07c
ivandjt226 <ivandjt226@gmail.com>
2023-11-24 02:54:32 -0500

    Estructura de pagina de chat

--- z_frontend/src/components/Chatbot.js
+++ z_frontend/src/components/Chatbot.js
@@ -2,85 +2,85 @@ import './chatbot.css';
 //import react from 'react';
 import { useEffect, useState } from 'react';
 
-import {IoMdSend}  from 'react-icons/io';
-import {BiBot,BiUser} from 'react-icons/bi';
+import { IoMdSend } from 'react-icons/io';
+import { BiBot, BiUser } from 'react-icons/bi';
 
 import logoE from '../static/Logo_Elaine.JPG'
 
-function Chatbot(){
-    const [chat,setChat] = useState([]);
-    const [inputMessage,setInputMessage] = useState('');
-    const [botTyping,setbotTyping] = useState(false);
+function Chatbot() {
+    const [chat, setChat] = useState([]);
+    const [inputMessage, setInputMessage] = useState('');
+    const [botTyping, setbotTyping] = useState(false);
 
-    
-   useEffect(()=>{
-   
+
+    useEffect(() => {
+
         console.log("called");
         const objDiv = document.getElementById('messageArea');
         objDiv.scrollTop = objDiv.scrollHeight;
-        
-    
-    },[chat])
-
-    
 
 
-    const handleSubmit=(evt)=>{
+    }, [chat])
+
+
+
+
+    const handleSubmit = (evt) => {
         evt.preventDefault();
         const name = "shreyas";
-        const request_temp = {sender : "user", sender_id : name , msg : inputMessage};
-        
-        if(inputMessage !== ""){
-            
+        const request_temp = { sender: "user", sender_id: name, msg: inputMessage };
+
+        if (inputMessage !== "") {
+
             setChat(chat => [...chat, request_temp]);
             setbotTyping(true);
             setInputMessage('');
-            rasaAPI(name,inputMessage);
+            rasaAPI(name, inputMessage);
         }
-        else{
+        else {
             window.alert("Por favor ingrese un mensaje valido");
         }
-        
+
     }
 
 
-    const rasaAPI = async function handleClick(name,msg) {
-    
-        //chatData.push({sender : "user", sender_id : name, msg : msg});
-        
+    const rasaAPI = async function handleClick(name, msg) {
 
-          await fetch('http://localhost:5005/webhooks/rest/webhook', {
+        //chatData.push({sender : "user", sender_id : name, msg : msg});
+
+
+        await fetch('http://localhost:5005/webhooks/rest/webhook', {
             method: 'POST',
             headers: {
-              'Accept': 'application/json',
-              'Content-Type': 'application/json',
-              'charset':'UTF-8',
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'charset': 'UTF-8',
             },
             credentials: "same-origin",
             body: JSON.stringify({ "sender": name, "message": msg }),
         })
-        .then(response => response.json())
-        .then((response) => {
-            if(response){
-                const temp = response[0];
-                const recipient_id = temp["recipient_id"];
-                const recipient_msg = temp["text"];        
+            .then(response => response.json())
+            .then((response) => {
+                if (response) {
+                    const temp = response[0];
+                    const recipient_id = temp["recipient_id"];
+                    const recipient_msg = temp["text"];
 
 
-                const response_temp = {sender: "bot",recipient_id : recipient_id,msg: recipient_msg};
-                setbotTyping(false);
-                
-                setChat(chat => [...chat, response_temp]);
-               // scrollBottom();
+                    const response_temp = { sender: "bot", recipient_id: recipient_id, msg: recipient_msg };
+                    setbotTyping(false);
 
-            }
-        }) 
+                    setChat(chat => [...chat, response_temp]);
+                    // scrollBottom();
+
+                }
+            })
     }
 
     console.log(chat);
 
     const stylecard = {
-        maxWidth : '35rem',
+        maxWidth: '35rem',
         border: '1px solid black',
         paddingLeft: '0px',
         paddingRight: '0px',
@@ -90,7 +90,7 @@ function Chatbot(){
     }
     const styleHeader = {
         height: '4.5rem',
-        borderBottom : '1px solid black',
+        borderBottom: '1px solid black',
         borderRadius: '30px 30px 0px 0px',
         backgroundColor: '#1b0527',
 
@@ -98,7 +98,7 @@ function Chatbot(){
     const styleHeaderLogo = {
         height: '3.5rem',
         width: '225px',
-        borderBottom : '1px solid black',
+        borderBottom: '1px solid black',
         backgroundColor: '#1b0527',
         paddingLeft: '0px',
         paddingRight: '0px',
@@ -108,110 +108,112 @@ function Chatbot(){
     }
     const styleFooter = {
         //maxWidth : '32rem',
-        borderTop : '1px solid black',
+        borderTop: '1px solid black',
         borderRadius: '0px 0px 30px 30px',
         backgroundColor: '#1b0527',
-        
-        
+
+
     }
     const styleBody = {
-        paddingTop : '10px',
+        paddingTop: '10px',
         height: '28rem',
         overflowY: 'a',
         overflowX: 'hidden',
-        
+
     }
     const styleBodyLogo = {
-        paddingTop : '10px',
+        paddingTop: '10px',
         height: '28rem',
         overflowY: 'a',
         overflowX: 'hidden',
-        
+
     }
 
     return (
-      <div>
-        {/* <button onClick={()=>rasaAPI("shreyas","hi")}>Try this</button> */}
-        
-        <br></br>
-        <div className="container">
-        <div className="row position-relative justify-content-flex-start">
+        <><h1 className="bg-info text-center font-monospace fw-bold lh-base">Chat de usuarios</h1>
+            <div className="d-flex flex-wrap justify-content-evenly">
+                {/* <button onClick={()=>rasaAPI("shreyas","hi")}>Try this</button> */}
 
-                    <div className="cardHeader text-white position-absolute top-0 start-0" style={styleHeaderLogo}>
-                    <h4 style={{marginBottom:'0px'}}>
-                          <center>Virtual Assistant</center>
-                    </h4>
-                    </div>
-                    <br></br>
-                    <div className="cardBody position-absolute top-100 start-0" style={styleBodyLogo}>
-                        <img
-                           src={logoE}
-                           width="200"
-                           height="300"
-                           className="d-inline-block align-center"
-                           alt="React Bootstrap logo"
-                        />{' '}
-
-                    </div>  
-  
-        </div>
-
-        <div className="row position-relative justify-content-center">
+                <br></br>
                 
-                <div className="card" style={stylecard} >
+                    <div className="">
 
-                    <div className="cardHeader text-white top-50 start-50" style={styleHeader}>
-                        <h1 style={{marginBottom:'0px'}}>
-                          <center>Elaine Assistant</center>
-                        </h1>
-                        {botTyping ? <h6>Elaine escribiendo...</h6> : null}
-                           
-                    </div>
-                    <div className="cardBody" id="messageArea" style={styleBody}>
-                        
-                        <div className="row msgarea">
-                            {chat.map((user,key) => (
-                                <div key={key}>
-                                    {user.sender==='bot' ?
-                                        (
-                                            
-                                            <div className= 'msgalignstart'>
-                                                <BiBot className="botIcon"  /><h5 className="botmsg">{user.msg}</h5>
-                                            </div>
-                                        
-                                        )
-
-                                        :(
-                                            <div className= 'msgalignend'>
-                                                <h5 className="usermsg">{user.msg}</h5><BiUser className="userIcon" />
-                                            </div>
-                                        )
-                                    }
-                                </div>
-                            ))}
-                            
+                        <div className="cardHeader text-white" style={styleHeaderLogo}>
+                            <h4 style={{ marginBottom: '0px' }}>
+                                <center>Virtual Assistant</center>
+                            </h4>
                         </div>
+                        <br></br>
+                        <div className="cardBody top-100 start-0" style={styleBodyLogo}>
+                            <img
+                                src={logoE}
+                                width="200"
+                                height="300"
+                                className="d-inline-block align-center"
+                                alt="React Bootstrap logo"
+                            />{' '}
+
+                        </div>
+
+                    </div>
+
+                    <div className="" style={{width:"700px"}}>
+
+                        <div className="card" style={stylecard} >
+
+                            <div className="cardHeader text-white top-50 start-50" style={styleHeader}>
+                                <h1 style={{ marginBottom: '0px' }}>
+                                    <center>Elaine Assistant</center>
+                                </h1>
+                                {botTyping ? <h6>Elaine escribiendo...</h6> : null}
+
+                            </div>
+                            <div className="cardBody" id="messageArea" style={styleBody}>
+
+                                <div className="row msgarea">
+                                    {chat.map((user, key) => (
+                                        <div key={key}>
+                                            {user.sender === 'bot' ?
+                                                (
+
+                                                    <div className='msgalignstart'>
+                                                        <BiBot className="botIcon" /><h5 className="botmsg">{user.msg}</h5>
+                                                    </div>
+
+                                                )
+
+                                                : (
+                                                    <div className='msgalignend'>
+                                                        <h5 className="usermsg">{user.msg}</h5><BiUser className="userIcon" />
+                                                    </div>
+                                                )
+                                            }
+                                        </div>
+                                    ))}
+
+                                </div>
+
+                            </div>
+                            <div className="cardFooter text-white" style={styleFooter}>
+                                <div className="row">
+                                    <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
+                                        <div className="col-10" style={{ paddingRight: '0px' }}>
+                                            <input onChange={e => setInputMessage(e.target.value)} value={inputMessage} type="text" className="msginp"></input>
+                                        </div>
+                                        <div className="col-2 cola">
+                                            <button type="submit" className="circleBtn" ><IoMdSend className="sendBtn" /></button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 
-                    </div>
-                    <div className="cardFooter text-white" style={styleFooter}>
-                        <div className="row">
-                            <form style={{display: 'flex'}} onSubmit={handleSubmit}>
-                                <div className="col-10" style={{paddingRight:'0px'}}>
-                                    <input onChange={e => setInputMessage(e.target.value)} value={inputMessage} type="text" className="msginp"></input>
-                                </div>
-                                <div className="col-2 cola">
-                                    <button type="submit" className="circleBtn" ><IoMdSend className="sendBtn" /></button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            
-        </div>
-        </div>
 
-      </div>
+            </div>
+        </>
     );
 }
-  
+
 export default Chatbot;
