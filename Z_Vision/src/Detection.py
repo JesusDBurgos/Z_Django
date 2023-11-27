3cca462345a6b1bd35b88850ad03668d35af9a45
ivandjt226 <ivandjt226@gmail.com>
2023-11-26 23:14:10 -0500

    Actualizacion de funcipon de detección

--- Z_Vision/src/Detection.py
+++ Z_Vision/src/Detection.py
@@ -2,14 +2,60 @@ import cv2
 from keras.models import load_model
 import numpy as np
 
-#Organización de rutas de modelos entrenados
+
+camara = cv2.VideoCapture(0)
+
+# Organización de rutas de modelos entrenados
 detection_model_path = './models/detection_models/haarcascade_frontalface_default.xml'
 emotion_model_path = './models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
 age_model_path = './models/age_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
 gender_model_path = './models/gender_models/simple_CNN.81-0.96.hdf5'
 
-# Cargamos los modelos
-face_detection = load_model(detection_model_path)
-emotion_classifier = load_model(emotion_model_path, compile=False)
-age_classifier = load_model(age_model_path, compile=False)
-gender_classifier = load_model(gender_model_path, compile=False)
+def get_frames_camera():
+
+    # Cargamos los modelos
+    face_detection = load_model(detection_model_path)
+    emotion_classifier = load_model(emotion_model_path, compile=False)
+    age_classifier = load_model(age_model_path, compile=False)
+    gender_classifier = load_model(gender_model_path, compile=False)
+
+    # Valores medibles
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    AgeList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)',
+           '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    AgeColors = [(0, 0, 255), (0, 255, 0), (0, 0, 0),
+             (255, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 255)]
+    GenderList = ['Male', 'Female']
+    EmotionList = ["angry", "disgust", "fear",
+               "happy", "sad", "suprised", "neutral"]
+    EmotionColors = [(0, 0, 255), (0, 255, 0), (0, 0, 0),
+                 (255, 255, 0), (255, 0, 0), (0, 255, 255), (255, 255, 255)]
+
+ 
+    #facec = cv2.CascadeClassifier('./models/detection_models/haarcascade_frontalface_default.xml')
+    facec = cv2.CascadeClassifier(detection_model_path)
+    #model = FacialExpressionModel("model.json", "model_weights.h5")
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    ok, frame = camara.read()
+    if not ok:
+        return False, None
+    
+    # Acá van los algoritmos de detección y clasificación
+    gray_fr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = facec.detectMultiScale(gray_fr, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        fc = gray_fr[y:y+h, x:x+w]
+
+        roi = cv2.resize(fc, (48, 48))
+        # pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+
+        # cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # Codificar la imagen como JPG
+        _, bufer = cv2.imencode(".jpg", frame)
+        imagen = bufer.tobytes()
+
+    return True, imagen
--- Z_Vision/src/camera.py
+++ Z_Vision/src/camera.py
@@ -35,6 +35,7 @@ class VideoCamera(object):
     # returns camera frames along with bounding boxes and predictions
     def get_frame(self):
         _, fr = self.video.read()
+        
         gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
         faces = facec.detectMultiScale(gray_fr, 1.3, 5)
 
@@ -42,9 +43,9 @@ class VideoCamera(object):
             fc = gray_fr[y:y+h, x:x+w]
 
             roi = cv2.resize(fc, (48, 48))
-            pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+            #pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
 
-            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+            #cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
             cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
 
         _, jpeg = cv2.imencode('.jpg', fr)
