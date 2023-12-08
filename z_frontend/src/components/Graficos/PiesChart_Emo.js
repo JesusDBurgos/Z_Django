b0af9b2a393b74c5e8052cd710990ccc2d7b6ea1
ivandjt226 <ivandjt226@gmail.com>
2023-12-07 23:29:10 -0500

    Vinculación de tag de emociones con la metrica de emociones

--- z_frontend/src/components/Graficos/PiesChart_Emo.js
+++ z_frontend/src/components/Graficos/PiesChart_Emo.js
@@ -5,39 +5,43 @@ import { Pie } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 var options = {
-    responsive : true,
+    responsive: true,
     maintainAspectRatio: false,
 };
 
-var data = {
-    labels: ['Felicidad', 'Sorpresa', 'Tristeza', 'Enojo', 'Miedo', 'Ira','Neutro'],
-    datasets: [
-        {
-            label: 'Emociones de los usuarios',
-            data: [10, 3, 5, 2, 5, 1, 5],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(0, 0, 192, 0.2)',
-                'rgba(0, 255, 0, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1,
-        },
-    ],
-};
+export default function Pies({ emociones }) {
 
-export default function Pies() {
+    if (!emociones || !emociones[1]) {
+        return null;
+    }
+    var data = {
+        labels: Object.keys(emociones[1]).filter(key => key !== 'Interested' && key !== 'Not Interested'),
+        datasets: [
+            {
+                label: 'Emociones de los usuarios',
+                data: Object.keys(emociones[1]).filter(key => key !== 'Interested' && key !== 'Not Interested').map(key => emociones[1][key][0]),
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(0, 0, 192, 0.2)',
+                    'rgba(0, 255, 0, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(54, 162, 235, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
+
     return <Pie data={data} options={options} />
 }
